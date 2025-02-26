a3ba57683ce49db46368375db4d76d215d8473c9
Durgesh Gupta <imdurgeshgupta@gmail.com>
2025-02-26 22:10:40 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/invoice-generator",
-    assetPrefix: "/invoice-generator",
     experimental: {
         serverComponentsExternalPackages: ["puppeteer-core"],
     },
